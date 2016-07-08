3aa7010d94612f01811152f3059149d6d636c0e3
Craig Rupp <craigrupp@utexas.edu>
2016-07-07 23:10:08 -0500

    Cleaner Code (No Longer Echo out of function)

--- internal_functions.php
+++ internal_functions.php
@@ -2,38 +2,39 @@
 
 // TODO: Create your inspect() function here
 
-function inspect($a){
-	// settype($num3, "string");
-	// echo gettype($num3), PHP_EOL;
-	// echo "The interger is 12.\n";
-	if(is_numeric($a)){
-		echo gettype($a), PHP_EOL;
-		echo "The " . gettype($a) . " is an " . $a . PHP_EOL;
+function inspect($value){
+	if(is_numeric($value)){
+		return "The " . gettype($value) . " is " . $value;
 	}
-	elseif(is_string($a) || is_scalar($a)){
-		echo gettype($a), PHP_EOL;
-		echo "The " . gettype($a) . " is an " . $a . PHP_EOL;
+	elseif (is_null($value)) {
+		return "The " . gettype($value) . " is NULL.";
 	}
-	elseif(is_string($a) && $a == ''){
-		echo gettype($a), PHP_EOL;
-		echo "The string is empty";
+	elseif(is_bool($value)){
+		if($value){
+			return "The boolean is true";
+		} else {
+			return "The boolean is false";
+		}
 	}
-	elseif(is_null($a)){
-		echo "The value is null\n";
-	} 
-	elseif(is_array($a) && $a == []){
-		echo "The value is an empty array\n";
-		// var_dump($a);
-	}elseif(is_array($a)){
-		var_dump($a);
-	}elseif(is_bool($a) && $a == true){
-		echo gettype($a), PHP_EOL;
-		echo "TRUE";
-	}elseif(is_bool($a) && $a == false){
-		echo gettype($a), PHP_EOL;
-		echo "FALSE";
+	elseif(is_string($value)){
+		if($value == ''){
+			return "The string is empty";
+		}
+		else {
+			return $value;
+		}
+	}
+	elseif(is_array($value)){
+		if($value == []){
+			return "The value is an empty array";
+		} else{
+			return "The " . gettype($value) . " is Array (" . join($value, ', ') . ")";
+		}
 	}
 }
+
+
+
 // Do not mofify these variables!
 $string1 = "I'm a little teapot";
 $string2 = '';
@@ -47,38 +48,30 @@ $num3 = 12;
 $num4 = 14.4;
 $null = NULL;
 
-inspect($string1);
-inspect($string2);
-inspect($array1);
-inspect($array2);
-inspect($bool1);
-inspect($bool2);
-inspect($num1);
-inspect($num2);
-inspect($num3);
-inspect($num4);
-inspect($null);
+
+
+//Echo outside of function
 
 // TODO: After each echo statement, use inspect() to output the variable's type and its value
 
-// echo 'Inspecting $num1:' . PHP_EOL;
+echo 'Inspecting $num1 : ' . inspect($num1) . PHP_EOL;
 
-// echo 'Inspecting $num2:' . PHP_EOL;
+echo 'Inspecting $num2 : ' . inspect($num2) . PHP_EOL;
 
-// echo 'Inspecting $num3:' . PHP_EOL;
+echo 'Inspecting $num3 : ' . inspect($num3) . PHP_EOL;
 
-// echo 'Inspecting $num4:' . PHP_EOL;
+echo 'Inspecting $num4 : ' . inspect($num4) . PHP_EOL;
 
-// echo 'Inspecting $null:' . PHP_EOL;
+echo 'Inspecting $null : ' . inspect($null) . PHP_EOL;
 
-// echo 'Inspecting $bool1' . PHP_EOL;
+echo 'Inspecting $bool1 : ' . inspect($bool1) . PHP_EOL;
 
-// echo 'Inspecting $bool2' . PHP_EOL;
+echo 'Inspecting $bool2 : ' . inspect($bool2) . PHP_EOL;
 
-// echo 'Inspecting $string1' . PHP_EOL;
+echo 'Inspecting $string1 : ' . inspect($string1) . PHP_EOL;
 
-// echo 'Inspecting $string2' . PHP_EOL;
+echo 'Inspecting $string2 : ' . inspect($string2) . PHP_EOL;
 
-// echo 'Inspecting $array1' . PHP_EOL;
+echo 'Inspecting $array1 : ' . inspect($array1) . PHP_EOL;
 
-// echo 'Inspecting $array2' . PHP_EOL;
+echo 'Inspecting $array2 : ' . inspect($array2) . PHP_EOL;
