b3b3667d801b17ab61a19ff141ca3723e1961ce3
Craig Rupp <craigrupp@utexas.edu>
2016-07-08 16:54:45 -0500

    New Document and quick ammend to previous doc

--- merge-arrays.php
+++ merge-arrays.php
@@ -41,3 +41,19 @@ function compareArrays ($array1, $array2) {
 }
 
 compareArrays($names, $compare);
+
+
+function combine_arrays($array1, $array2){
+	$newArray = [];
+	for ($i = 0; $i < count($array1); $i++){
+		if($array1[$i] === $array2[$i]){
+			$newArray[] = $array1[$i];
+		} else{
+			$newArray[] = $array1[$i];
+			$newArray[] = $array2[$i];
+		}
+	}
+	return $newArray;
+}
+
+var_dump(combine_arrays($names, $compare));
