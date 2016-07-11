b11d03b84cd695e08d2c87fd90fc07bba7e44a93
Craig Rupp <craigrupp@utexas.edu>
2016-07-11 15:39:05 -0500

    Modify and start Logger

--- contact-parser.php
+++ contact-parser.php
@@ -3,19 +3,20 @@
 function parseContacts($filename)
 {
     $contacts = [];
-    $thisFilename = "{$filename}";
-    $handle = fopen($thisFilename, 'r');
-    $contents = fread($handle, filesize($thisFilename));
+    $handle = fopen($filename, 'r');
+    $contents = fread($handle, filesize($filename));
+    fclose($handle);
     $person = explode("\n", trim($contents));
     foreach ($person as $contact) {
       $info = explode("|", $contact);
+   	  // var_dump($person);
+      $info[1] = substr($info[1], 0, 3) . "-" . substr($info[1], 3, 3) . "-" . substr($info[1], 6);
       $eachContact = [
         'name' => $info[0],
         'number' => $info[1]
       ];
       $contacts[] = $eachContact;
     };
-    fclose($handle);
     return $contacts;
 }
 var_dump(parseContacts('contacts.txt'));
