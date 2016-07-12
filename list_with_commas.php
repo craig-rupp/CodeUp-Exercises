e349f8cfde2396cff08b147dbbd80d33fbe2ca44
Craig Rupp <craigrupp@utexas.edu>
2016-07-11 21:03:10 -0500

    Cleaner List with Commas

--- list_with_commas.php
+++ list_with_commas.php
@@ -3,32 +3,23 @@
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-$physicistsArray = explode(', ', $physicistsString);
+$autoSort = false;
 
-// print_r($physicistsArray);
-
-// $and = "and";
-sort($physicistsArray);
-// print_r($physicistsArray);
-
-array_splice($physicistsArray, 5, 0, 'and');
-// print_r($physicistsArray);
-
-
-
-print_r($physicistsArray);
-
-$famousFakePhysicists = ' ';
-
-function humanizedList($famousFakePhysicists){
-
-	global $physicistsArray;
+function humanizedList($physicistsArray, $autoSort = false){
+	if($autoSort){
+		sort($physicistsArray);	
+	}
+	array_pop($physicistsArray);
+	array_splice($physicistsArray, 5, 0, 'and Tony Stark');
 
 	$famousFakePhysicists = implode(', ', $physicistsArray);
-
-	echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+	
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
 
 }
+$physicistsArray = explode(', ', $physicistsString);
 
-humanizedList($famousFakePhysicists);
+humanizedList($physicistsArray, true);
+
+
 
